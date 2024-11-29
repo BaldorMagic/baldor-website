397dab8035e8c6a8a57b5ba06e8ed16cb13b2f98
Baldor <james@baldor.dev>
2024-11-29 16:42:45 +0100

    creating my website

---
+++ nuxt.config.ts
@@ -0,0 +1,16 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+  modules: [
+    '@nuxt/image',
+    '@nuxt/icon',
+    '@nuxt/ui',
+    '@nuxtjs/tailwindcss',
+    '@vueuse/nuxt',
+    'nuxt-directus'
+  ],
+  image: {
+    //options
+  }
+})
